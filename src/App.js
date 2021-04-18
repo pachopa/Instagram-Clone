1f2ccb82baddfb0515789a9c411437f3e4cf572e
Daechul <dlee@nuundigital.com>
2021-04-17 22:51:22 -0600

    create sign-up and login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/sign-up'));
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+
         </Switch>
       </Suspense>
     </Router>
